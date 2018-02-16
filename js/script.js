9cad8926b4108a34b70ef86a962e3101bf4b37a2
elwira-m <36117564+elwira-m@users.noreply.github.com>
2018-02-16 01:56:34 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//script.js
+
+var a = 2,
+	b = 3,
+	value = (a * a) - (2 * a * b) - (b * b);
+
+console.log (value);
+
+if (value > 0) {
+		console.log ('wynik dodatni');
+}
+else if (value < 0) {
+	console.log ('wynik ujemny');
+}
+else {
+	console.log ('wynik = 0');
+}
